5cbe357e3f82c1dac921f02bff565cc836f96d6f
Akanksha Unde <akankshaunde07863@gmail.com>
2022-01-25 14:22:01 +0530

    Updates

---
+++ precache-manifest.c70c237b0cb1c3d5b2eb2c43f50088db.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e188ddafa7981b86ee5f52953bf60a",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "e739d53e00ed842bf853",
+    "url": "/keeper-app/static/js/2.33895965.chunk.js"
+  },
+  {
+    "revision": "d955e5a3c32d07b1b475",
+    "url": "/keeper-app/static/js/main.eec06807.chunk.js"
+  },
+  {
+    "revision": "e4ad95b7ee73d89007ed",
+    "url": "/keeper-app/static/js/runtime-main.fc0473d0.js"
+  }
+]);
