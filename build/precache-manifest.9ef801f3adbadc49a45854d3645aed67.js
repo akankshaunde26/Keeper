3d0aa2c7d380b64647eae810408e9707bc4678cc
Akanksha Unde <akankshaunde07863@gmail.com>
2022-01-25 14:13:11 +0530

    my first commit

---
+++ build/precache-manifest.9ef801f3adbadc49a45854d3645aed67.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335f2bf7c880f6a56b0a60dc19959d89",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "e739d53e00ed842bf853",
+    "url": "/keeper/static/js/2.33895965.chunk.js"
+  },
+  {
+    "revision": "d955e5a3c32d07b1b475",
+    "url": "/keeper/static/js/main.eec06807.chunk.js"
+  },
+  {
+    "revision": "092074f7cd57a7d0f215",
+    "url": "/keeper/static/js/runtime-main.1e0fb4f8.js"
+  }
+]);
